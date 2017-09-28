5edb264b9005375f47fad4f6f0e60d825b58196f
mallika-mewar <m@ne1013>
2017-09-28 17:46:34 +0200

    Generated microservice version 1.0

---
+++ db/upgradingmicroservice_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  microservicetab
+-- Automatically generated sql script for the service upgradingmicroservice, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table microservicetab.
+--
+CREATE TABLE microservicetab.microservicetab (
+  microId INTEGER ,
+CONSTRAINT microId_PK PRIMARY KEY (microId)
+ 
+);
+
+
+
